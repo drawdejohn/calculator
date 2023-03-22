4a50fbe9b4acae8d8c15bedbb2f07f58a169bfb2
Edward John Felicia <76100222+edwardjohnfelicia@users.noreply.github.com>
2023-03-22 13:51:53 +0800

    Fix decimal precision, add comment to each function and link to styles.css

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
-
-//A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
-let firstNum="";
-let operator="";
-let secondNum="";
+// Initialize variables to hold the numbers, operator, display value, and flags
+let firstNum = "";
+let operator = "";
+let secondNum = "";
 let displayValue = "";
+let isDecimal = false;
+let evaluated = false;
+let lastClickedOperator = false;
 
-//Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+// Define the operate function to perform arithmetic operations
 function operate(operator, x, y) {
   switch (operator) {
     case "add":
@@ -22,47 +23,53 @@ function operate(operator, x, y) {
   }
 }
 
-//Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step?
-
-// Get references to the calculator buttons
-const buttonIds = ["btn7", "btn8", "btn9", "btn4", "btn5", "btn6", "btn1", "btn2", "btn3", "btn0", "equals", "clear", "plus", "minus", "times", "dividedBy"];
+// Initialize the buttons object and assign the button elements to their respective keys
 const buttons = {};
-
+const buttonIds = ["btn7", "btn8", "btn9", "btn4", "btn5", "btn6", "btn1", "btn2", "btn3", "btn0", "equals", "clear", "plus", "minus", "times", "dividedBy"];
 buttonIds.forEach(id => {
   buttons[id] = document.getElementById(id);
 });
 
-let evaluated = false;
-let lastClickedOperator = false;
-
-equals.addEventListener("click", () => {
-  if (operator){
-    if(!lastClickedOperator){
-      secondNum = displayValue;
-      const result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
-      displayValue = result.toString();
-      updateDisplay();
-      firstNum="";
-      secondNum="";
-      evaluated = true;
-      lastClickedOperator = false;
-      isDecimal = false;
-      operator="";
-    }
-  } 
-});
-
-clear.addEventListener("click", () => {
-  displayValue = "";
-  firstNum = "";
-  secondNum = "";
-  operator = "";
+// Define the clearDisplay function to reset all variables and update the display
+const clearDisplay = () => {
+  displayValue = '';
+  firstNum = '';
+  secondNum = '';
+  operator = '';
   evaluated = false;
   lastClickedOperator = false;
   isDecimal = false;
   updateDisplay();
-  });
+};
 
+// Add a click listener to the clear button to call the clearDisplay function
+clear.addEventListener('click', clearDisplay);
+
+// Add a click listener to the equals button to process the calculation
+equals.addEventListener("click", () => {
+  if (operator) {
+    if (!lastClickedOperator) {
+      processDisplay();
+      firstNum = "";
+      lastClickedOperator = false;
+      operator = "";
+    }
+  } 
+});
+
+// Define processDisplay function to perform calculation and update display and flags
+function processDisplay() {
+  let precision = 9;
+  secondNum = displayValue;
+  let result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
+  result = Math.floor(result * Math.pow(10, precision)) / Math.pow(10, precision);
+  displayValue = result.toString();
+  secondNum="";
+  evaluated = true;
+  isDecimal = false;
+  updateDisplay();
+}
+// Define handleOperatorClick function to handle click event for operator buttons
 function handleOperatorClick(op) {
   if (!lastClickedOperator){
     if (firstNum === "") {
@@ -71,15 +78,9 @@ function handleOperatorClick(op) {
       lastClickedOperator = true;
       isDecimal = false;
     } else {
-      secondNum = displayValue;
-      const result = operate(operator, parseFloat(firstNum), parseFloat(secondNum));
-      firstNum = result.toString();
-      displayValue = result.toString();
-      updateDisplay();
-      secondNum="";
-      evaluated = true;
+      processDisplay();
+      firstNum = displayValue;
       lastClickedOperator = true;
-      isDecimal = false;
     }
     operator = op;
   } else {
@@ -88,6 +89,7 @@ function handleOperatorClick(op) {
 
 }
 
+// Add click event listener to each operator button to call the handleOperatorClick function
 const operatorButtons = {
   plus: "add",
   minus: "subtract",
@@ -100,8 +102,7 @@ for (const [buttonId, operator] of Object.entries(operatorButtons)) {
     button.addEventListener("click", () => handleOperatorClick(operator));
 }
 
-let isDecimal = false;
-
+// Define addClickListener function to add click listener to buttons and update display accordingly
 function addClickListener(button, value) {
   button.addEventListener("click", () => {
     if (button == decimal) {
@@ -114,6 +115,7 @@ function addClickListener(button, value) {
   });
 }
 
+// Update displayed numbers when a digit or decimal point is clicked
 function updateNumbers(value) {
   if (evaluated) {
     displayValue = "";
@@ -128,23 +130,14 @@ function updateNumbers(value) {
   }
 }
 
+// Assign click listeners to buttons for each value in the values array
 const btns = [btn7, btn8, btn9, btn4, btn5, btn6, btn1, btn2, btn3, btn0, decimal];
 const values = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
 for (let i = 0; i < btns.length; i++) {
     addClickListener(btns[i], values[i]);
 }
 
+//Updates the calculator display with the current result or input value
 function updateDisplay() {
-
-  let precision = 9;
-  
-  if (displayValue=="") {
     display.textContent = displayValue;
-  } else if (displayValue.includes(".")){
-    display.textContent = displayValue;
-  } else {
-    let num = parseFloat(displayValue)
-    displayValue = Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision)
-    display.textContent = displayValue.toString();
-  }
 }
